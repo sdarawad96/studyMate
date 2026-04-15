a9ccac1a8d4b2878cabcdddd090b39f3e8d2efd7
sdarawa1 <s_darawad96@yahoo.com>
2026-04-14 22:31:41 -0500

    renames classes and Added StudySSession class to model package

--- src/main/java/edu/westga/comp2320/studymate/StudyMateController.java
+++ src/main/java/edu/westga/comp2320/studymate/StudyMateController.java
@@ -3,7 +3,7 @@ package edu.westga.comp2320.studymate;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StudyMateController {
     @FXML
     private Label welcomeText;
 
---
+++ src/main/java/edu/westga/comp2320/studymate/model/StudySession.java
@@ -0,0 +1,4 @@
+package edu.westga.comp2320.studymate.model;
+
+public class StudySession {
+}
